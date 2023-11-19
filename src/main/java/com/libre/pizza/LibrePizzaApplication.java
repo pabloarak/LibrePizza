c0c49a64ce32a653935e28eeb3e59f1778d7e857
Pablo Arak <pablo.arak.vm@gmail.com>
2023-11-19 18:14:30 -0300

    Initial file

---
+++ src/main/java/com/libre/pizza/LibrePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.libre.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrePizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrePizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libre/pizza/LibrePizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libre.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrePizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
